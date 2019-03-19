85c0e9116c0a066c8e9dd5bf70cfccbf2a7bf059
andrei-walber <andrei.walber@gmail.com>
2019-03-18 23:09:39 -0300

    curso laravel 5.5 terminado

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UpdateProfileFormRequest;
 
 class UserController extends Controller
 {
@@ -13,14 +14,38 @@ class UserController extends Controller
         return view('site.profile.profile');
     }
 
-    public function profileUpdate(Request $request)
+    public function profileUpdate(UpdateProfileFormRequest $request)
     {
+        $user = auth()->user();
+
         $data = $request->all();
 
         if ($data['password'] != null) {
             $data['password'] = bcrypt($data['password']);
         } else {
             unset($data['password']);
+        }
+
+        $data['image'] = $user->image;
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            if ($user->image) {
+                $name = $user->image;
+            } else {
+                $name = kebab_case($user->name).'-'.$user->id;
+            }
+
+            $extension = $request->image->extension();
+            $nameFile = "{$name}.{$extension}";
+
+            $data['image'] = $nameFile;
+
+            $upload = $request->image->storeAs('users', $nameFile);
+
+            if (!$upload) {
+                return redirect()
+                    ->back()
+                    ->with('error', 'Falha ao fazer o upload');
+            }
         }
 
         $update = auth()->user()->update($data);
